b9ebf1279b4ec5f28595d79270aca168672638a3
maciej.zebala <maciej.zebala@setapp.pl>
2021-10-26 19:28:50 +0200

    Intial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    lightGrey: '#F7F8FA',
+    grey: '#DFE2E8',
+    darkGrey: '#737C8E',
+    black: '#111111',
+    success: '#8FCB81',
+    error: '#FF8383',
+  },
+  fontSize: {
+    xxl: '34px',
+    xl: '24px',
+    l: '17px',
+    m: '12px',
+    s: '11px',
+  },
+};
